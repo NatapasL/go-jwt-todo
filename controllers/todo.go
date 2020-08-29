3d0a3285744a3185ae514c11aa0cbaf1344a08d8
natapas <natapas.lue@gmail.com>
2020-08-29 18:28:31 +0700

    Add todo controller

---
+++ controllers/todo.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"go-jwt-todo/forms"
+	"go-jwt-todo/persistences/postgres"
+	"go-jwt-todo/services"
+)
+
+type TodoController struct {
+	DB string
+}
+
+func (controller *TodoController) Create(c *gin.Context) {
+	var params forms.CreateTodoParams
+	if err := c.ShouldBindJSON(&params); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "invalid json")
+		return
+	}
+	access, ok := c.Get("access_details")
+	if !ok {
+		c.JSON(http.StatusUnauthorized, "unauthorized")
+	}
+	accessDetails, ok := access.(services.AccessDetails)
+	if !ok {
+		c.JSON(http.StatusUnauthorized, "unauthorized")
+	}
+	params.UserID = accessDetails.UserId
+
+	todoRepository := persistences.NewPostgresTodoRepository(controller.DB)
+	todo, err := todoRepository.Create(params)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusCreated, todo)
+}
