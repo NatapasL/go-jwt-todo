88c8fdb8215939553f47b9e89d243df635ab0b62
natapas <natapas.lue@gmail.com>
2020-08-29 18:28:39 +0700

    Add authentication middleware

---
+++ middlewares/authentication.go
@@ -0,0 +1,36 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v7"
+
+	"go-jwt-todo/services"
+)
+
+func AuthenticationMiddleware(r *redis.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := extractToken(c.Request)
+		verifyTokenService := services.AuthenticationService{Redis: r}
+		accessDetails, err := verifyTokenService.VerifyToken(token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, "unauthorized")
+			c.Abort()
+			return
+		}
+
+		c.Set("access_details", *accessDetails)
+		c.Next()
+	}
+}
+
+func extractToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+	strArr := strings.Split(token, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
+}
