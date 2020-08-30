b966aa2942ff665be8b4626220dc7b076268ccc0
natapas <natapas.lue@gmail.com>
2020-08-31 00:53:02 +0700

    Load env variable to variable

--- infra/gorm.go
+++ infra/gorm.go
@@ -2,6 +2,7 @@ package infra
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -11,25 +12,26 @@ import (
 
 var db *gorm.DB
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "postgres"
-	dbname   = "test_test"
-)
-
 func ConnectToDb() {
+	var (
+		host     = os.Getenv("DB_HOST")
+		port     = os.Getenv("DB_PORT")
+		user     = os.Getenv("DB_USER")
+		password = os.Getenv("DB_PASSWORD")
+		dbname   = os.Getenv("DB_NAME")
+	)
 	var err error
 
 	dbInfo := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host,
 		port,
 		user,
 		password,
 		dbname,
 	)
+
+	fmt.Println(dbInfo)
 	db, err = gorm.Open("postgres", dbInfo)
 	if err != nil {
 		panic(err)
